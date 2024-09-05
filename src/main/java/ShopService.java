19c5580cc394f8e7eb827142b9e788b983b85533
Phaete <pel4nk@gmail.com>
2024-09-05 10:26:35 +0200

    Change getProductById in ProductRepo to return an optional of a product or an empty one if the product does not exist Create a ProductDoesNotExistException to reflect what kind of problem occured, namely the product not existing Change addOrder in ShopService to incorporate the Optinal<Product> from getProductById and throw a ProductDoesNotExistException if the Optional is empty Fix test in ProductRepoTest to handle the change to Optional<Product> instead of just a Product when testing with getProductById

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,24 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws ProductDoesNotExistException {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+            if(!productRepo.getProductById(productId).isPresent()) {
+                throw new ProductDoesNotExistException("Product mit der Id: " + productId + " existiert nicht und konnte nicht bestellt werden!");
             }
+            Product productToOrder = productRepo.getProductById(productId).get();
             products.add(productToOrder);
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> getOrderByStatus(OrderStatus status) {
+        return orderRepo.getOrders().stream()
+                .filter(order -> order.status() == status)
+                .toList();
     }
 }
